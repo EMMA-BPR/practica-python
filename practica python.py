adf85250b2b41d7ee7de9fe404520cc0b576202e
EMMA-BPR <145418806+EMMA-BPR@users.noreply.github.com>
2024-04-04 20:50:16 -0600

    Add files via upload

---
+++ practica python.py
@@ -0,0 +1,111 @@
+
+"células" : [
+  {
+   "tipo_celda" : " código " ,
+   "recuento_ejecución" : 9 ,
+   "metadatos" : {},
+   "salidas" : [
+    {
+     "nombre" : " salida estándar " ,
+     "tipo_salida" : " transmisión " ,
+     "texto" : [
+      " ¿Qué fecha es hoy? \n " ,
+      " ¿En qué fecha naciste? \n " ,
+      " no es tu cumplaños ctm \n "
+     ]
+    }
+   ],
+   "fuente" : [
+    " print( \" ¿Qué fecha es hoy? \" ) \n " ,
+    " fecha = input( \" Ingresa una fecha \" ) \n " ,
+    " print( \" ¿En qué fecha naciste? \" ) \n " ,
+    " fecha_de_nacimiento = input( \" Ingresa tu fecha de nacimiento \" ) \n " ,
+    " if fecha == fecha_de_nacimiento: \n " ,
+    "     print( \" Es tu cumplaños \" ) \n " ,
+    " más: \n " ,
+    "     print( \" no es tu cumplaños ctm \" ) \n " ,
+    " \n " ,
+    "    \n " ,
+    " \n "
+   ]
+  },
+  {
+   "tipo_celda" : " código " ,
+   "recuento_ejecución" : 10 ,
+   "metadatos" : {},
+   "salidas" : [
+    {
+     "nombre" : " salida estándar " ,
+     "tipo_salida" : " transmisión " ,
+     "texto" : [
+      " ingresa un numero \n " ,
+      " ingresa otro numero \n " ,
+      " 25 \n " ,
+      " 25 \n " ,
+      " son valores iguales \n "
+     ]
+    }
+   ],
+   "fuente" : [
+    " print( \" ingresa un numero \" ) \n " ,
+    " numero1 = int(input( \" ingresa el numero 5 \" )) \n " ,
+    " print( \" ingresa otro numero \" ) \n " ,
+    " numero2 = int(input ( \" ingresa el numero 10 \" )) \n " ,
+    " valor1 = (numero1*5) \n " ,
+    " valor2 = (numero2+15) \n " ,
+    " imprimir (valor1) \n " ,
+    " imprimir (valor2) \n " ,
+    " si valor1 == valor2: \n " ,
+    "     print( \" son valores iguales \" ) \n " ,
+    " \n " ,
+    " \n " ,
+    " \n "
+   ]
+  },
+  {
+   "tipo_celda" : " código " ,
+   "recuento_ejecución" : 8 ,
+   "metadatos" : {},
+   "salidas" : [
+    {
+     "nombre" : " salida estándar " ,
+     "tipo_salida" : " transmisión " ,
+     "texto" : [
+      " ingresa tu codigo postal \n " ,
+      " esta dentro del rango de entrega \n "
+     ]
+    }
+   ],
+   "fuente" : [
+    " print( \" ingresa tu código postal \" ) \n " ,
+    " cp = int(input( \" ingresa tu cp \" )) \n " ,
+    " si 50000 <= cp <=50030: \n " ,
+    "     print( \" esta dentro del rango de entrega \" ) \n " ,
+    " más: \n " ,
+    "     print( \" no esta dentro del rango de entrega ya te jodiste \" ) \n " ,
+    " \n " ,
+    " \n "
+   ]
+  }
+], # type: ignore
+"metadatos" : {
+  "especificación del núcleo" : {
+   "display_name" : " Python 3 " ,
+   "idioma" : " pitón " ,
+   "nombre" : " python3 "
+  },
+  "información_idioma" : {
+   "codemirror_mode" : {
+    "nombre" : " ipython " ,
+    "versión" : 3
+   },
+   "extensión_archivo" : " .py " ,
+   "mimetype" : " texto/x-python " ,
+   "nombre" : " pitón " ,
+   "nbconvert_exporter" : " pitón " ,
+   "pygments_lexer" : " ipython3 " ,
+   "versión" : " 3.11.7 "
+  }
+}, # type: ignore
+ "nbformato" : 4 , # type: ignore
+ "nbformat_minor" : 2 # type: ignore
